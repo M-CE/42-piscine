9a1c3520b2bae3cc0e16e9c6934c307b9c22f3a0
Mary <marie-charlotte@gmx.de>
2020-08-25 19:52:09 +0200

    Create r_dwssap.sh

---
+++ C Piscine Shell 00/CS01/ex07/r_dwssap.sh
@@ -0,0 +1,13 @@
+export FT_LINE1=7
+export FT_LINE2=9
+
+cat /etc/passwd | \
+	grep -v -e "#" | \
+	awk "NR % 2 == 0" | \
+	cut -d':' -f1 | \
+	rev | \
+	sort -rn | \
+	awk "FNR>=$FT_LINE1 && FNR<=$FT_LINE2" | \
+	sed 's/$/,/' | \
+	tr '\n' ' ' | \
+	sed 's/, *$/\./'
