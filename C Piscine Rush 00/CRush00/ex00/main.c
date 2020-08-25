475d1eaffc3e6d317e3ff17d7605d7f6a796e91a
Mary <marie-charlotte@gmx.de>
2020-08-25 19:53:47 +0200

    Create main.c

---
+++ C Piscine Rush 00/CRush00/ex00/main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mfreiin- <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/08/16 13:05:37 by mfreiin-      #+#    #+#                 */
+/*   Updated: 2020/08/17 20:19:30 by mfreiin-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+// function prototypes to link to rush file since #linking is not allowed
+void	print_horizontal(int x, char left, char middle, char right);
+void	print_vertical(int x, int y, char sym);
+void	rush(int x, int y);
+void	ft_putchar(char c);
+
+// function that takes x, y input for rush to get output (symbools, letters depending on rush.c functions)
+int		main(void)
+{
+}
